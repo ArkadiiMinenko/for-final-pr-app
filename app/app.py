bb59a10d92892c864ed2f0e6d6c6d95c30d7965e
ArkadiiMinenko <arkadii.minenko@gmail.com>
2025-07-27 21:54:55 +0300

    Initial app + Dockerfile

---
+++ app/app.py
@@ -0,0 +1,19 @@
+from http.server import BaseHTTPRequestHandler, HTTPServer
+import socket
+
+class SimpleHandler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        if self.path == "/":
+            pod_ip = socket.gethostbyname(socket.gethostname())
+            self.send_response(200)
+            self.send_header("Content-Type", "text/plain; charset=utf-8")
+            self.end_headers()
+            self.wfile.write(f"OK\nPod IP: {pod_ip}\n".encode())
+        else:
+            self.send_error(404)
+
+if __name__ == "__main__":
+    PORT = 8000
+    server = HTTPServer(("", PORT), SimpleHandler)
+    print(f"Listening on 0.0.0.0:{PORT}")
+    server.serve_forever()
