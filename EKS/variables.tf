448551256fb755db195452cb1c729861f4f5a3dd
ArkadiiMinenko <arkadii.minenko@gmail.com>
2025-07-29 23:04:38 +0300

    perezalivO4ka5

--- EKS/variables.tf
+++ EKS/variables.tf
@@ -1,42 +1,44 @@
 variable "name" {
-    default = "arkadii"
+  default = "arkadii"
 }
+
 #variable "vpc_cidr" {
 #}
 #variable "private_subnets" {
 #}
 #variable "public_subnets" {
 #}
+
 variable "vpc_id" {
-}
-variable "subnets_ids" {
-}
-variable "tags" {
-}
-variable "region" {
-  description = "aws region"
-  default     = "eu-central-1"
+  type = string
 }
 
-### Backend vars
+variable "subnets_ids" {
+  type = list(string)
+}
+
+variable "tags" {
+  type = map(string)
+}
+
+variable "region" {
+  default = "eu-central-1"
+}
+
 variable "iam_profile" {
-  description = "Profile of aws creds"
-  default     = null
+  type    = string
+  default = null
 }
 
 variable "zone_name" {
+  type = string
 }
 
 variable "external_dns_policy_name" {
   type    = string
-<<<<<<< HEAD
   default = "external-dns-irsa-external-dns-arkadii"
 }
 
 variable "admin_password_bcrypt" {
-  description = "bcrypt-хеш пароля для ArgoCD admin"
-  type        = string
-=======
-  default = "external-dns-irsa-external-dns"
->>>>>>> c4b68d66763e7a055b833a6708ed193cd3f73e87
+  type = string
 }
