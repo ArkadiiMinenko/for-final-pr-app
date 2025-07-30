bdb2120ca1f2fc1ec03f5bfa41712ca3662ee1ce
ArkadiiMinenko <arkadii.minenko@gmail.com>
2025-07-30 07:22:54 +0300

    final test of all

--- EKS/modules/argocd/main.tf
+++ EKS/modules/argocd/main.tf
@@ -5,7 +5,6 @@ resource "helm_release" "argocd" {
   chart            = "argo-cd"
   version          = "5.46.8"
   create_namespace = true
-  
 
   values = [
     templatefile("${path.module}/values.yaml", {
@@ -32,6 +31,14 @@ data "kubernetes_ingress_v1" "argocd_ingress" {
   depends_on = [helm_release.argocd]
 }
 
+resource "null_resource" "update_kubeconfig" {
+  provisioner "local-exec" {
+    command = "aws eks update-kubeconfig --region eu-central-1 --name arkadii"
+  }
+
+  depends_on = [module.eks_cluster]
+}
+
 resource "null_resource" "apply_argocd_app" {
   depends_on = [
     helm_release.argocd,
@@ -39,16 +46,6 @@ resource "null_resource" "apply_argocd_app" {
   ]
 
   provisioner "local-exec" {
-    command = "kubectl apply -f ${path.module}/app.yaml"
+    command = "kubectl apply -f ${path.module}/app.yaml --validate=false"
   }
 }
-
-
-resource "null_resource" "update_kubeconfig" {
-  provisioner "local-exec" {
-    command = "aws eks update-kubeconfig --region eu-central-1 --name arkadii"
-  }
-
-  depends_on = [aws_eks_cluster.danit]
-}
-
